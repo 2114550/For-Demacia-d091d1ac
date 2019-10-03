3e0661c7f549947e4b04561d884fefba5504bb62
2114550 <2114550@talnet.nl>
2019-10-03 09:10:51 +0200

    Uitwerking van de exercise

---
+++ for_klas.php
@@ -0,0 +1,11 @@
+<?php
+$var = array(
+    "Sjonnie de Wiel",
+    "Herman Kaal",
+    "Henk de Steen",
+    "Inge Kerkhoven",
+    "Gert Kruiswijk",
+);
+foreach ($var as $var1) {
+    echo "$var1".PHP_EOL;
+}
